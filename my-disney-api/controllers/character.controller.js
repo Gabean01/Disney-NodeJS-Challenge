626fc376bce434dbaf681bff69203b02422c97f0
Evert Moreno <gbeanrenteria@gmail.com>
2023-08-17 14:54:03 -0500

    Created character controller

---
+++ my-disney-api/controllers/character.controller.js
@@ -0,0 +1,137 @@
+const req = require("express/lib/request");
+const db = require("../models");
+const res = require("express/lib/response");
+const Character = db.character;
+const Op = db.Sequelize.Op;
+
+//Create and Save a new Character
+exports.create = (req, res) => {
+    if (!req.body.name) {
+        res.status(400).send({
+            message: "Name can not be empty!"
+        });
+        return;
+    }
+    const character = {
+        name: req.body.name,
+        age: req.body.age,
+        weight: req.body.weight,
+        history: req.body.history,
+        image: req.body.image
+    };
+
+    Character.create(character)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Character."
+            });
+        });
+};
+
+exports.findAll = (req, res) => {
+    const character_name = req.query.character_name;
+    var condition = character_name ? { name: {
+            [Op.like]: `%${character_name}%` } } : null;
+
+    Character.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving characters."
+            });
+        });
+};
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Character.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Character with id=" + id
+            });
+        });
+};
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Character.update(req.body, {
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Character was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Character with id=${id}. Maybe Character was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Character with id=" + id
+            });
+        });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Character.destroy({
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Character was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Character with id=${id}. Maybe Character was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Character with id=" + id
+            });
+        });
+};
+
+exports.deleteAll = (req, res) => {
+    Character.destroy({
+            where: {},
+            truncate: false
+        })
+        .then(nums => {
+            res.send({ message: `${nums} Characters were deleted successfully!` });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while removing all characters."
+            });
+        });
+};
+
+exports.findAllPublished = (req, res) => {
+    Character.findAll({ where: { published: true } })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving characters."
+            });
+        });
+};
